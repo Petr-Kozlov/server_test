f72d900d243399a4b8122400f97ab2c0989b78df
Petr-Kozlov <97740485+Petr-Kozlov@users.noreply.github.com>
2024-01-07 23:52:37 +0900

    Add Mysql

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 
 const app = new express();
 
 const PORT = process.env.PORT ?? 3400;
 
-app.get('/', function(req, res){
-    res.send('Hello server');
+const connectionPool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "server_test",
+    password: "2wbOB84t1pfKc4a",
+    connectionLimit: 20,
+});
+
+app.get('/', async function(req, res){
+    const data = await connectionPool.query(`SELECT * FROM users`);
+    res.json(data[0]);
 });
 
 app.listen(PORT, () => {
